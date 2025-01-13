4550b30c350a3509a5732eee1849977060b186b3
nmoussaoui <nassim.moussaoui@ecoles-epsi.net>
2025-01-13 15:39:03 +0100

    Add Firebase authentication setup to firebaseConfig

--- App/src/firebaseConfig.ts
+++ App/src/firebaseConfig.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDKbxes9OZdiviX0dblbml1eSOzDK-oNwg",
@@ -12,5 +13,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
 export { db };
+export { auth };
